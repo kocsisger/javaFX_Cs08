8738fbd6f3e77dc6bb04bd830ece652c24be4e8f
Kocsis Gergely <kocsisger@example.com>
2025-09-25 09:21:31 +0200

    Controller connected

--- src/main/java/org/example/MainController.java
+++ src/main/java/org/example/MainController.java
@@ -1,8 +1,17 @@
 package org.example;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class MainController {
+    @FXML
+    private Label seasonsLabel;
+
+    @FXML
+    void handleClick() {
+
+    }
+
     @FXML
     private void initialize() {
 
